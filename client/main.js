9c7aca965a084d215665b1b55483f2dd20891ccc
Gang Fan <gang.fan@uconn.edu>
2016-08-25 14:50:07 -0400

    Running on mobile

--- client/main.js
+++ client/main.js
@@ -6,3 +6,16 @@ angular.module('simple-todos', [
   angularMeteor,
   todosList.name
 ]);
+
+
+// Running on mobile
+
+function onReady() {
+  angular.bootstrap(document, ['simple-todos']);
+}
+ 
+if (Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
